fa8ca4953dd5666389fb0b20da77e7cc1f260598
jaebradley <jae.b.bradley@gmail.com>
2016-12-28 14:55:54 -0500

    added some things

---
+++ src/data/TimestampUnit.js
@@ -0,0 +1,8 @@
+'use es6';
+
+export default class TimestampUnit extends Enum {};
+
+TimestampUnit.initEnum([
+  'SECOND',
+  'MILLISECOND',
+]);
--- src/services/UnixTimestampService.js
+++ src/services/UnixTimestampService.js
@@ -1,5 +1,9 @@
 'use es6';
 
+import moment from 'moment-timezone';
+
 export default class UnixTimestampService {
-  
+  static getCurrentTimestamp() {
+    return moment().valueOf();
+  }
 }
