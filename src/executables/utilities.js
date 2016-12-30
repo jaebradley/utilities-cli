eed83cb550a66ebec280e45e24a9ec78d168cd8f
jaebradley <jae.b.bradley@gmail.com>
2016-12-29 19:35:40 -0500

    add timestamp executable

--- src/executables/utilities.js
+++ src/executables/utilities.js
@@ -2,4 +2,8 @@
 
 'use es6';
 
-import program from "commander";
+import program from 'commander';
+
+program.version('0.0.1')
+       .command('timestamp', 'get timestamp')
+       .parse(process.argv);
