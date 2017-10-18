f3a58fa6c3f62820874f231088aaf7da23a727a1
jaebradley <jae.b.bradley@gmail.com>
2017-10-18 19:39:13 -0400

    lint utilities execution service

--- src/services/UtilitiesExecutionService.js
+++ src/services/UtilitiesExecutionService.js
@@ -2,38 +2,38 @@ import ncp from 'copy-paste';
 import moment from 'moment-timezone';
 
 import FormattedDateTime from '../data/FormattedDateTime';
-import TimeUnit from '../data/TimeUnit';
 import TimeTableCreator from './tables/TimeTableCreator';
-import DecodedUriTableCreator from './tables/DecodedUriTableCreator';
-import EncodedUriTableCreator from './tables/EncodedUriTableCreator';
 import SingleValueTableCreator from './tables/SingleValueTableCreator';
 
 export default class UtilitiesExecutionService {
   static executeTimestampCommand(unit, copyToClipboard) {
     const dateTime = new FormattedDateTime({
       instant: moment(),
-      unit: unit
+      unit,
     });
 
     const timestamp = dateTime.getFormattedTimestamp();
 
     if (copyToClipboard) {
-      ncp.copy(timestamp, console.log(UtilitiesExecutionService.createCopyToClipboardMessage(timestamp)));
+      const clipboardMessage = UtilitiesExecutionService.createCopyToClipboardMessage(timestamp);
+      ncp.copy(timestamp, console.log(clipboardMessage)); // eslint-disable-line no-console
     }
 
-    console.log(TimeTableCreator.createTimestampTable(dateTime));
+    console.log(TimeTableCreator.createTimestampTable(dateTime)); // eslint-disable-line no-console
   }
   static executeUriDecodeCommand(uri, copyToClipboard) {
     try {
       const decodedValue = decodeURIComponent(uri);
 
       if (copyToClipboard) {
-        ncp.copy(decodedValue, console.log(UtilitiesExecutionService.createCopyToClipboardMessage(decodedValue)));
+        const clipboardMessage = UtilitiesExecutionService
+          .createCopyToClipboardMessage(decodedValue);
+        ncp.copy(decodedValue, console.log(clipboardMessage)); // eslint-disable-line no-console
       }
 
-      console.log(SingleValueTableCreator.create('Decoded Value', decodedValue));
+      console.log(SingleValueTableCreator.create('Decoded Value', decodedValue)); // eslint-disable-line no-console
     } catch (e) {
-      console.error('Could not decode URI');
+      console.error('Could not decode URI'); // eslint-disable-line no-console
     }
   }
 
@@ -42,12 +42,14 @@ export default class UtilitiesExecutionService {
       const encodedValue = encodeURIComponent(uri);
 
       if (copyToClipboard) {
-        ncp.copy(encodedValue, console.log(UtilitiesExecutionService.createCopyToClipboardMessage(encodedValue)));
+        const clipboardMessage = UtilitiesExecutionService
+          .createCopyToClipboardMessage(encodedValue);
+        ncp.copy(encodedValue, console.log(clipboardMessage)); // eslint-disable-line no-console
       }
 
-      console.log(SingleValueTableCreator.create('Encoded Value', encodedValue));
+      console.log(SingleValueTableCreator.create('Encoded Value', encodedValue)); // eslint-disable-line no-console
     } catch (e) {
-      console.error('Could not encode URI');
+      console.error('Could not encode URI'); // eslint-disable-line no-console
     }
   }
 
