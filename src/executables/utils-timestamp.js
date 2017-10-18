73106bc4461b32030ba500c83d76142cd6910b45
jaebradley <jae.b.bradley@gmail.com>
2017-10-18 19:42:09 -0400

    lint the utils commands

--- src/executables/utils-timestamp.js
+++ src/executables/utils-timestamp.js
@@ -5,18 +5,19 @@ import program from 'commander';
 import TimeUnit from '../data/TimeUnit';
 import UtilitiesExecutionService from '../services/UtilitiesExecutionService';
 
-program.option('-m, --millisecond', 'Return value in milliseconds')
-       .option('-c, --copyToClipboard', 'Copy value to clipboard')
-       .parse(process.argv);
+program
+  .option('-m, --millisecond', 'Return value in milliseconds')
+  .option('-c, --copyToClipboard', 'Copy value to clipboard')
+  .parse(process.argv);
 
-const unit = !!program.millisecond
-   ? TimeUnit.MILLISECOND
-   : TimeUnit.SECOND;
+const unit = !program.millisecond
+  ? TimeUnit.MILLISECOND
+  : TimeUnit.SECOND;
 
 const copyToClipboard = !!program.copyToClipboard;
 
- try {
-   UtilitiesExecutionService.executeTimestampCommand(unit, copyToClipboard);
- } catch (Error) {
-   console.error('Could not get timestamp');
- }
+try {
+  UtilitiesExecutionService.executeTimestampCommand(unit, copyToClipboard);
+} catch (Error) {
+  console.error('Could not get timestamp'); // eslint-disable-line no-console
+}
